ebbe1a70647a8a61cde189a6207ec2670e17a97c
Eddie Howell <eihowell@syr.edu>
2026-01-25 20:24:58 -0500

    Refactor app to use navigation and multiple labs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+Lab1 = st.Page('labs/lab1.py',
+    title = "Lab 1",
+    icon = "📄",
+    url_path = None,
+    default = False)
+Lab2 = st.Page('labs/lab2.py',
+    title = "Lab 2",
+    icon = "🥼",
+    url_path = None,
+    default = False)
+
+pg = st.navigation ( [Lab1, Lab2])
+st.set_page_config(page_title='Lab Manager')
+pg. run ()
