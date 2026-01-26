f0025190893e3d5691fde9cc4cdf276084ed0add
Eddie Howell <eihowell@syr.edu>
2026-01-25 20:28:50 -0500

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,12 @@
 import streamlit as st
 
-Lab1 = st.Page('labs/lab1.py',
-    title = "Lab 1",
+Lab1 = st.Page('hws/hw1.py',
+    title = "HW 1",
     icon = "📄",
     url_path = None,
     default = False)
-Lab2 = st.Page('labs/lab2.py',
-    title = "Lab 2",
-    icon = "🥼",
-    url_path = None,
-    default = False)
 
-pg = st.navigation ( [Lab1, Lab2])
+
+pg = st.navigation ( [Lab1])
 st.set_page_config(page_title='Lab Manager')
 pg. run ()
