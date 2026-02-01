17e0d6a5f7b73aea5b875816cde0d06edd53922f
Eddie Howell <eihowell@syr.edu>
2026-02-01 17:28:05 -0500

    HW2 adding Claude

--- hws/hw2.py
+++ hws/hw2.py
@@ -3,6 +3,7 @@ from openai import OpenAI
 from pypdf import PdfReader
 from bs4 import BeautifulSoup
 import requests
+import anthropic
 
 # Show title and description.
 st.title("HW 2")
@@ -10,6 +11,7 @@ st.write(
     "Enter a url below and ask a question about it – GPT will answer! "
 )
 st.sidebar.title("HW 2 Settings")
+add_LLM_sbox = st.sidebar.selectbox("Which LLM would you like to use?", ["ChatGPT", "Claude"])
 add_sum_sbox = st.sidebar.selectbox("Which kind of summary would you like to use?", ["in 100 Words", "in 2 Paragraphs", "in 5 Bullet Points"])
 add_lang_sbox = st.sidebar.selectbox("Output language", ["English", "French", "Spanish"])
 add_model_checkbox = st.sidebar.checkbox("Would you like to use the stonger model?", value = False)
@@ -29,14 +31,12 @@ def read_url_content(url):
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
 openai_api_key = st.secrets.EddieOpenAPIKey
+claude_api_key = st.secrets.EddieClaudeAPIKey
 
-client = OpenAI(api_key=openai_api_key)
 
-try:
-    client.models.list()
-    st.success(f"API Key Success")
-except Exception as e:
-    st.error(f"API Key Failed: {e}")
+anthropic_client = anthropic.Anthropic(api_key=claude_api_key)
+openAI_client = OpenAI(api_key=openai_api_key)
+
 
 # Let the user upload a file via `st.file_uploader`.
 uploaded_link = st.text_input("Enter a URL")
@@ -47,16 +47,27 @@ if uploaded_link:
 
     document = read_url_content(uploaded_link)
 
-    if add_model_checkbox:
-        model_check = "gpt-5-mini"
-    else:
-        model_check = "gpt-5-nano"
-
-    response = client.responses.create(
+    if add_LLM_sbox == "ChatGPT":
+        if add_model_checkbox:
+            model_check = "gpt-5-mini"
+        else:
+            model_check = "gpt-5-nano"
+        response = openAI_client.responses.create(
         model=model_check,
         input=f"Here is a document:\n{document}\n\nSummarize this document {add_sum_sbox}. Respond in {add_lang_sbox}.",
         stream=True,
     )
+    else:
+        if add_model_checkbox:
+            model_check = "claude-sonnet-4-5-20250929"
+        else:
+            model_check = "claude-haiku-4-5-20251001"
+        response = anthropic_client.responses.create(
+            model=model_check,
+            input=f"Here is a document:\n{document}\n\nSummarize this document {add_sum_sbox}. Respond in {add_lang_sbox}.",
+            stream=True,
+        )
+
 
     def stream_text():
         for event in response:
